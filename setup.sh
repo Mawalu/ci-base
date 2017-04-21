b786e510e6f822a31b85f06f6ce3c19531f915f5
mawalu <web@mawalabs.de>
2017-04-21 22:53:57 +0200

    Install zip php extension

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@ apt-get update && apt-get upgrade -y
 apt-get install -y git openssh-client zip curl libpng12-dev libgd-dev
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
-docker-php-ext-install pdo_mysql gd
+docker-php-ext-install pdo_mysql gd zip
 pecl install xdebug > /dev/null
 docker-php-ext-enable xdebug
