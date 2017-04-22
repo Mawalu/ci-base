0e6ae54296fae6235d22f77da065eba69c047bb2
mawalu <web@mawalabs.de>
2017-04-22 23:11:10 +0200

    Install xvfb and node.js

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-apt-get update && apt-get upgrade -y
-apt-get install -y git openssh-client zip curl libpng12-dev libgd-dev
+
+# install node.js
+curl -sL https://deb.nodesource.com/setup_7.x | bash -
+
+# install php
+apt-get upgrade -y
+apt-get install -y git openssh-client zip curl libpng12-dev libgd-dev nodejs xvfb
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
+# install php extensions
 docker-php-ext-install pdo_mysql gd zip
 pecl install xdebug > /dev/null
 docker-php-ext-enable xdebug
