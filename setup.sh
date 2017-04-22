a12378bc44dda35838e0e57cfd9032661c6356a3
mawalu <web@mawalabs.de>
2017-04-23 00:00:22 +0200

    Install yarn

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-# install node.js
+# install node.js and yarn
 curl -sL https://deb.nodesource.com/setup_7.x | bash -
 
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+
 # install php
-apt-get upgrade -y
-apt-get install -y git openssh-client zip curl libpng12-dev libgd-dev nodejs xvfb
+apt-get update && apt-get upgrade -y
+apt-get install -y git openssh-client zip curl libpng12-dev libgd-dev nodejs xvfb yarn
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
 # install php extensions
