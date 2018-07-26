e8781ce5329ef7c39ea7877fa2e6f248a1720a62
Tobias Dierich <git@tobiasdierich.de>
2018-07-26 14:57:15 +0200

    Install imagick

--- setup.sh
+++ setup.sh
@@ -22,6 +22,11 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
 echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
 apt-get update && apt-get install -y google-chrome-unstable
 
+# imagick
+export CFLAGS="$PHP_CFLAGS" CPPFLAGS="$PHP_CPPFLAGS" LDFLAGS="$PHP_LDFLAGS" \
+    && apt-get update \
+    && apt-get install -y --no-install-recommends libmagickwand-dev
+
 # set memory limit
 echo "memory_limit=1024M" > /usr/local/etc/php/conf.d/memory-limit.ini
 
@@ -29,5 +34,5 @@ echo "memory_limit=1024M" > /usr/local/etc/php/conf.d/memory-limit.ini
 docker-php-ext-configure intl
 docker-php-ext-install pdo_mysql zip intl pdo_pgsql soap xml gd bcmath
 
-pecl install xdebug > /dev/null
-docker-php-ext-enable xdebug bcmath
+pecl install xdebug pecl install imagick-3.4.3 > /dev/null
+docker-php-ext-enable xdebug bcmath imagick
