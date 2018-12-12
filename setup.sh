a2d6166e5edcd61dfece9dabffe0076d8c712ca2
mawalu <web@mawalabs.de>
2018-12-12 22:33:19 +0100

    xdebug-beta is required for php 7.3

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.lis
 
 # install dependencies
 apt-get update && apt-get upgrade -y
-apt-get install -y git openssh-client zip curl libpng-dev libgd-dev nodejs xvfb yarn netcat libnss3 libgconf-2-4 wget zlib1g-dev libicu-dev g++ libpq-dev libxml2-dev libpng-dev libjpeg62-turbo-dev libfreetype6-dev libwebp-dev libxpm-dev iproute2
+apt-get install -y git openssh-client libzip-dev zip curl libpng-dev libgd-dev nodejs xvfb yarn netcat libnss3 libgconf-2-4 wget zlib1g-dev libicu-dev g++ libpq-dev libxml2-dev libpng-dev libjpeg62-turbo-dev libfreetype6-dev libwebp-dev libxpm-dev iproute2
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
 # install quasar cli
@@ -35,5 +35,5 @@ docker-php-ext-configure intl
 docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/
 docker-php-ext-install pdo_mysql zip intl pdo_pgsql soap xml gd bcmath exif pcntl
 
-pecl install xdebug pecl install imagick-3.4.3 > /dev/null
+pecl install xdebug-beta pecl install imagick-3.4.3 > /dev/null
 docker-php-ext-enable xdebug bcmath imagick
